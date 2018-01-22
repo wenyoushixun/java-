43f21a7651a2cd01e636723c40385caa5b580a45
register_strutsmvc <wenyou@192.168.1.99>
2018-01-22 14:03:10 +0800

    orm

---
+++ ORM/src/main/java/com/qingshixun/project/crm/module/complaint/dao/ComplaintDao.java
@@ -0,0 +1,5 @@
+package com.qingshixun.project.crm.module.complaint.dao;
+
+public class ComplaintDao {
+
+}
---
+++ ORM/src/main/java/com/qingshixun/project/crm/module/login/service/LoginService.java
@@ -0,0 +1,31 @@
+/*****************************************************************************
+ * Copyright (c) 2015, www.qingshixun.com
+ *
+ * All rights reserved
+ *
+ *****************************************************************************/
+
+package com.qingshixun.project.crm.module.login.service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import com.qingshixun.project.crm.core.BaseService;
+
+/**
+ * 登录处理 Service 类
+ * 
+ * @author QingShiXun
+ * 
+ * @version 1.0
+ */
+@Service
+@Transactional
+public class LoginService extends BaseService {
+
+    // 注入登录处理Dao
+//    @Autowired
+//    private LoginDao loginDao;
+}
+
